01abda72bb1e6c2d43df2345b996f17543d7c9cb
sanketchheladiya <mychance2001@gmail.com>
2022-01-21 11:33:01 +0530

    Initial commit

---
+++ src/main/java/com/example/demoecom/DemoecomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoecom/DemoecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
